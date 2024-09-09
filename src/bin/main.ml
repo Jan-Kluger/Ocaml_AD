25abb7fd196ba3d21165d371009e7d20bb13ef98
yannick <yannick@schuermann.org>
2024-09-09 19:11:58 +0200

    commiting rest

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -2,28 +2,34 @@
 open Queue_lib.List_queue
 open Sa_lib.Sorted_array_list
 open Sort_lib.List_mergesort *)
-open Heap_imp_lib.Heap_imp
+open Avl_tree_lib.Avl_tree
 
 let () = print_endline "stack_str"
-let int_comparator x y = (compare x y)>0
+(* let int_comparator x y = (compare x y)>0 *)
 
-let heap =
-  Node (1,
-    Node (2,
-      Node (4, Nil, Nil),
-      Node (5, Nil, Nil)),
-    Node (3,
-      Node (6, Nil, Nil),
-      Nil))
+let avl_tree = Nil
 
-let heap = Heap_imp.insert heap 7 int_comparator
+let avl_tree = Avl_tree.insert avl_tree 5
 
-let heap = Heap_imp.delete_min heap int_comparator
+let avl_tree = Avl_tree.insert avl_tree 8
 
-let heap_to_print = 
+let avl_tree = Avl_tree.insert avl_tree 2
+
+let avl_tree = Avl_tree.insert avl_tree 3
+
+let avl_tree = Avl_tree.insert avl_tree 4
+
+let () = print_endline (Avl_tree.toString avl_tree string_of_int)
+
+
+(* let heap = Heap_imp.insert heap 7 int_comparator
+
+let heap = Heap_imp.delete_min heap int_comparator *)
+
+(* let heap_to_print = 
   match heap with
   | Some tree -> tree
-  | _ -> failwith "No tree to print"
+  | _ -> failwith "No tree to print" *)
 
 
 (* let to_print = 
@@ -31,8 +37,8 @@ match test_val with
 | Node (value, _, _) -> value
 | _ -> failwith "this shouldnt happen" *)
 
-let () =
-  print_endline (Heap_imp.to_string heap_to_print string_of_int)
+(* let () =
+  print_endline (Heap_imp.to_string heap_to_print string_of_int) *)
 
 
 
