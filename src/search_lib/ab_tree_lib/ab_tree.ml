909c883d8effffeca2e107ba8b622093b0f7d31a
yannick <yannick@schuermann.org>
2024-09-09 19:10:31 +0200

    updated return type

--- src/search_lib/ab_tree_lib/ab_tree.ml
+++ src/search_lib/ab_tree_lib/ab_tree.ml
@@ -7,7 +7,7 @@ module Ab_Tree : Search_lib.Search_sig.SEARCH with type 'a t = 'a tree = struct
 
   let insert (avl_tree : 'a t) (element : 'a ) : 'a t = failwith "TODO"
   let remove (avl_tree : 'a t) (element : 'a ) : 'a t = failwith "TODO"
-  let locate (avl_tree : 'a t) (element : 'a ) : 'a = failwith "TODO"
+  let locate (avl_tree : 'a t) (element : 'a ) : 'a option = failwith "TODO"
   let toString  (avl_tree : 'a t) (to_string : ('a -> string)) : string = failwith "TODO"
   
 end
