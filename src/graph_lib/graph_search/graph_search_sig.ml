9d0e3b98a021f44241868dd2f0aa50272e08003e
yannick <yannick@schuermann.org>
2024-09-18 18:56:24 +0200

    added hash function to sig, since i oculdnt find a good fix

--- src/graph_lib/graph_search/graph_search_sig.ml
+++ src/graph_lib/graph_search/graph_search_sig.ml
@@ -1,5 +1,5 @@
 module type SEARCH = sig
   type 'a graph
 
-  val search : graph:'a graph -> start:'a -> dest:'a -> 'a list option
+  val search : graph:'a graph -> start:'a -> dest:'a -> hash_function:('a -> int) -> 'a list option
 end
