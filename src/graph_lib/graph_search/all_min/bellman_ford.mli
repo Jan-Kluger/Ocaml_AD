798f09fc72866d295a55c6998dc383d333ae813e
yannick <yannick@schuermann.org>
2024-09-20 19:28:58 +0200

    added necessary hash sig in module interface

--- src/graph_lib/graph_search/all_min/bellman_ford.mli
+++ src/graph_lib/graph_search/all_min/bellman_ford.mli
@@ -1,4 +1,4 @@
-module type BellmanFord = sig
+module BellmanFord (_: Hash_lib.Hash_sig.HASH_SIG with type ('a, 'b) t = ('a * 'b) list array) : sig
     type 'a graph
   
     val has_negative_cycle : graph: ('a graph) -> start: ('a) -> hash_function: ('a -> int) -> bool
